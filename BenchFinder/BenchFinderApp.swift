a72cb17ab80dcec7fa170d5b0323617e1e4ca4f6
Dominik Helbling <dominik.helbling@ubs.com>
2022-06-20 16:46:40 +0200

    Initial Commit

---
+++ BenchFinder/BenchFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  BenchFinderApp.swift
+//  BenchFinder
+//
+//  Created by Dominik Helbling on 20.06.22.
+//
+
+import SwiftUI
+
+@main
+struct BenchFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
